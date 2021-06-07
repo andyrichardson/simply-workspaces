5be52a2a404af0898068446e322602d9259276ae
Test Richardson <email>
2021-06-07 20:14:24 +0100

    Initial commit

---
+++ extension.js
@@ -0,0 +1,76 @@
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const { Container, WorkspaceIndicator } = Me.imports.ui;
+
+const WorkspaceManager = global.workspace_manager;
+const Main = imports.ui.main;
+
+// Initial state
+
+let indicators;
+let container;
+const handlers = [];
+
+const initUI = () => {
+  const currentWorkspace = WorkspaceManager.get_active_workspace_index();
+  indicators = new Array(9).fill(null).map((_, i) => {
+    const active = i === currentWorkspace;
+    const workspace = WorkspaceManager.get_workspace_by_index(i);
+    return WorkspaceIndicator({
+      label: `${i + 1}`,
+      active,
+      windowCount: workspace.n_windows,
+    });
+  });
+  container = Container({ indicators });
+};
+
+const attachHandlers = () => {
+  // Listen for workspace switch
+  const changeHandler = WorkspaceManager.connect("active-workspace-changed", (...args) => {
+    const activeWorkspace = WorkspaceManager.get_active_workspace_index();
+    indicators.forEach((indicator, index) => {
+      index === activeWorkspace ? indicator.setActive() : indicator.setInactive();
+    });
+  });
+  handlers.push(() => WorkspaceManager.disconnect(changeHandler));
+
+  const workspaceHandlers = indicators.map((instance, i) => {
+    const workspace = WorkspaceManager.get_workspace_by_index(i);
+    const updateWindowCount = () => instance.setWindowCount(workspace.n_windows);
+
+    const windowAdded = workspace.connect("window-added", updateWindowCount);
+    const windowRemoved = workspace.connect("window-removed", updateWindowCount);
+    const nodeClick = instance.node.connect('clicked', () => {
+      log('CLICK DETECTED')
+      workspace.activate(Date.now()/1000)
+    });
+
+    return () => {
+      workspace.disconnect(windowAdded);
+      workspace.disconnect(windowRemoved);
+      instance.disconnect(nodeClick);
+    };
+  });
+  handlers.concat(workspaceHandlers);
+};
+
+const detachHandlers = () => {
+  while(handlers.length > 0) {
+    handlers.pop()();
+  }
+};
+
+var init = (meta) => {
+  log("Initializing extension");
+};
+
+var enable = (meta) => {
+  initUI();
+  attachHandlers();
+  Main.panel._leftBox.insert_child_at_index(container.node, 0);
+};
+
+var disable = (meta) => {
+  detachHandlers();
+  Main.panel._leftBox.remove_child(container.node)
+};
---
+++ ui.js
@@ -0,0 +1,70 @@
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const { St } = imports.gi;
+
+
+var WorkspaceIndicator = ({ label, active, windowCount }) => {
+  const node = St.Button.new_with_label(label);
+  const state = {
+    label,
+    active,
+    windowCount,
+  };
+
+  node.set_style_class_name('indicator');
+  const render = () => {
+    if (!state.active && !state.windowCount) {
+      node.hide();
+      return;
+    }
+
+    const styles = ['indicator', state.active ? 'active' : ''];
+    node.set_style_class_name(styles.join(' '));
+    node.show();
+    log(node.get_style_class_name())
+  };
+
+  const setLabel = (l) => (state.label = l);
+
+  const setActive = () => {
+    state.active = true;
+    render();
+  };
+  
+  const setInactive = () => {
+    state.active = false;
+    render();
+  };
+
+  const setWindowCount = (n) => {
+    state.windowCount = n;
+    render();
+  };
+
+  // Initial render
+  render();
+
+  return {
+    node,
+    setActive,
+    setInactive,
+    setWindowCount,
+    setLabel,
+  };
+};
+
+var Container = ({ indicators }) => {
+  const node = St.BoxLayout.new();
+  node.set_style_class_name('container');
+  
+  indicators.forEach((i) => {
+    node.add_child(i.node);
+  });
+
+  return {
+    node,
+    indicators
+  };
+};
+
+var styleToString = (style) =>
+  Object.keys(style).reduce((agg, key) => `${agg} ${key}: ${style[key]};`, "");
